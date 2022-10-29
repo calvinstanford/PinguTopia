372caa8c540e3664ad08ae53e74e52213f75e546
cieloren <roquid.1217@outlook.com>
2022-10-29 16:09:01 +1300

    testing sprite changer script

---
+++ Assets/Scripts/SpriteImageUpdate.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteImageUpdate : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite sp1,sp2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (spriteRenderer.sprite != sp1)
+            {
+                spriteRenderer.sprite = sp1;
+            }
+            else
+            {
+                spriteRenderer.sprite = sp2;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteUpdater.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class SpriteUpdater : MonoBehaviour
+// {
+//     public Sprite sp1,sp2;
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if(Input.GetMouseButtonDown(0))
+//             {
+//                 this.gameObject.GetComponent<SpriteRenderer>().sprite = sp1;
+//             }
+//         else {
+//             null;
+//         }
+//         // if(Input.GetMouseButtonDown(0))
+//         //     GetComponent<SpriteRenderer>().Sprite = nurturersp;  
+//     }
+// }
+
+
